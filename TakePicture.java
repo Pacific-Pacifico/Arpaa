e8c2a93bd8dedd94d700e01f44223cc3cc5aa998
Prashant Pacific <30399370+Pacific-Pacifico@users.noreply.github.com>
2019-04-08 21:21:42 +0530

    webcam package

---
+++ TakePicture.java
@@ -0,0 +1,25 @@
+package webcam;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import com.github.sarxos.webcam.Webcam;
+
+public class TakePicture {
+
+	public static void main(String[] args) throws IOException {
+
+		// get default webcam and open it
+		Webcam webcam = Webcam.getDefault();
+		webcam.open();
+
+		// get image
+		BufferedImage image = webcam.getImage();
+
+		// save image to PNG file
+		ImageIO.write(image, "PNG", new File("test.png"));
+	}
+}
